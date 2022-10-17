05e6a2b84030112b118a6801297c56a3309a2430
Daniel Venturini <daniel.venturini@objective.com.br>
2022-10-17 20:10:07 -0300

    add base project

---
+++ test/main/java/com/jwatcher/app/AppTest.java
@@ -0,0 +1,10 @@
+package com.jwatcher.app;
+
+import static org.junit.Asert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+	public void shouldAnswerWithTrue () {
+		assertTrue(3 * 4 == 12);
+	}
+}
